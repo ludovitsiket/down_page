bb7df600d55792aa609c70ce182d8d0ff8ec0ebb
Ludo <siket.ludovit@gmail.com>
2020-01-31 09:55:01 +0100

    Nice code according PEP8.
    Translate to english.

--- down_page.py
+++ down_page.py
@@ -15,56 +15,56 @@ def check_correct_url(url):
 
 
 def help_syntax():
-    print('Skript vyzaduje nainstalovany python 3.x')
-    print('Skript si url adresy nacita zo suboru. Kazda adresa musi byt na samostatnom riadku.')
+    print('Script need python 3.x')
     print('Syntax (GNU/Linux, macOS) : python3 down_page.py -f')
-    print('Syntax (Windows)          : python.exe down_page.py -f')
-    print('Bez parametru -f sa ako parameter pouzije 1 url adresa ktora sa ma stiahnut.')
+    print('Syntax (Windows): python.exe down_page.py -f')
+    print('Without parameter -f is posible using one url adress:')
     print('Syntax (GNU/Linux, macOS) : python3 down_page.py www.web_page.com')
-    print('Syntax (Windows)          : python.exe down_page.py www.web_page.com')
+    print('Syntax (Windows): python.exe down_page.py www.web_page.com')
 
 
-def write_to_log(error, log_file):
+def write_log(error, log_file):
     print(error)
     log(log_file)
 
 
 def log(log_file):
-    with open(log_file, "a+") as lg:
-        formated_date_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
-        lg.write(
-            "[  " + formated_date_time + "  ]" + "  :" + "  " + str(sys.exc_info()[0]) + str(sys.exc_info()[1]) + '\n')
+    with open(log_file, "a+") as log:
+        date_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        log.write(
+            "[  " + date_time + "  ]" + "  :" + "  " + str(sys.exc_info()[0]) +
+            str(sys.exc_info()[1]) + '\n')
 
 
-def make_aimed_directory(directory, log_file):
+def make_dir(directory, log):
     try:
         os.mkdir(directory)
     except Exception as e:
-        write_to_log(e, log_file)
+        write_log(e, log)
 
 
-def download_web_page_data(url, log_file):
+def download_data(url, log):
     try:
         content = requests.get(url)
         content.encoding = 'utf-8'
         data = content.text
         return data
     except Exception as e:
-        write_to_log(e, log_file)
+        write_log(e, log)
 
 
-def save_web_page_content(data, downloaded_file, log_file):
+def save_content(data, content, log):
     try:
-        with open(downloaded_file, "w") as local_page:
-            local_page.write(data)
-        return local_page
+        with open(content, "w") as local:
+            local.write(data)
+        return local
     except Exception as e:
-        write_to_log(e, log_file)
+        write_log(e, log)
 
 
-def check_for_changes(destination, temporary_html):
+def check_for_changes(dest, temp):
     diff = []
-    with open(destination, "r") as local, open(temporary_html, "r") as actual:
+    with open(dest, "r") as local, open(temp, "r") as actual:
         for local_line, temp_line in zip(local, actual):
             if local_line != temp_line:
                 if '<script' in temp_line or '</script>' in temp_line:
@@ -74,52 +74,52 @@ def check_for_changes(destination, temporary_html):
     return diff
 
 
-def compare_web_page_content(url, destination, temporary_html, log_file):
-    print("Web stranka uz je stiahnuta. Porovnavam jej obsah s aktualnou online verziou. Cakajte prosim.")
-    actual_content = download_web_page_data(url, log_file)
-    save_web_page_content(actual_content, temporary_html, log_file)
-    diff = check_for_changes(destination, temporary_html)
+def compare_content(url, dest, temp, log):
+    print("Web page is downloaded. Comparing with actual online version.")
+    actual_content = download_data(url, log)
+    save_content(actual_content, temp, log)
+    diff = check_for_changes(dest, temp)
     if len(diff) != 0:
-        print('Doslo k zmenam na web stranke.\nBude stiahnuta jej aktualna verzia.')
-        save_web_page_content(actual_content, destination, log_file)
+        print('Offline web page is old.\nDownloading actual version.')
+        save_content(actual_content, dest, log)
     else:
-        print('Ziadna zmena.')
+        print('Without changes.')
 
 
-def find_images_on_page(data, log_file):
+def find_images(data, log):
     try:
         img = findall('img .*?src="(.*?)"', data)
         return img
     except Exception as e:
-        write_to_log(e, log_file)
+        write_log(e, log)
 
 
 def join_path(directory, output_file):
     return os.path.join(directory, output_file)
 
 
-def create_file_name(directory, picture):
+def create_file_name(directory, pic):
     if True:
-        name = join_path(directory, picture).replace("/", "")
+        name = join_path(directory, pic).replace("/", "")
         name = os.path.join(directory, name)
         name = name.replace(":", "")
         return name
 
 
-def check_picture_url(url, picture):
-    if "http" in picture:
-        picture = picture
+def check_picture_url(url, pic):
+    if "http" in pic:
+        pic = pic
     else:
-        picture = (url + picture)
-    return picture
+        pic = (url + pic)
+    return pic
 
 
-def base64_picture_download(url, local_picture):
-    picture = urllib.request.urlopen(url).read()
-    picture_64_encode = b64encode(picture)
-    picture_64_decode = b64decode(picture_64_encode)
-    with open(local_picture, 'wb') as pic:
-        pic.write(picture_64_decode)
+def base64_pic_down(url, local_picture):
+    pic = urllib.request.urlopen(url).read()
+    pic_64_encode = b64encode(pic)
+    pic_64_decode = b64decode(pic_64_encode)
+    with open(local_picture, 'wb') as picture:
+        picture.write(pic_64_decode)
 
 
 def download_local(local_file, images, url, directory):
@@ -137,25 +137,24 @@ def download_remote(remote_file, images):
             remote_urls.write(image + '\n')
 
 
-def download_images_from_web_page(directory, data, url, log_file):
+def download_images(directory, data, url, log):
     try:
-        images = find_images_on_page(data, log_file)
+        images = find_images(data, log)
         local_file, remote_file = stored_data(directory)
         download_local(local_file, images, url, directory)
         download_remote(remote_file, images)
         for image in images:
             image = check_picture_url(url, image)
-            picture_name = create_file_name(directory, image)
+            pic_name = create_file_name(directory, image)
             try:
                 if "base64" in image:
-                    base64_picture_download(image, picture_name)
+                    base64_pic_down(image, pic_name)
                 else:
-                    urllib.request.urlretrieve(image, picture_name)
+                    urllib.request.urlretrieve(image, pic_name)
             except (ValueError, urllib.error.URLError):
                 pass
-
     except Exception as e:
-        write_to_log(e, log_file)
+        write_log(e, log)
 
 
 def stored_data(directory):
@@ -184,18 +183,18 @@ def write_data_per_line(some_file, data):
     return f
 
 
-def get_and_change_data_in_files(file1, file2):
-    result1, result2 = change_data_in_memory(file1, file2)
+def change_files_data(file1, file2):
+    result1, result2 = change_data(file1, file2)
     write_data_per_line(file1, result1)
     write_data_per_line(file2, result2)
     return file1, file2
 
 
-def change_data_in_memory(file1, file2):
+def change_data(file1, file2):
     data1, data2 = read_data(file1, file2)
-    dictionary = collections.OrderedDict(zip(data1, data2))
-    inverted_dict = collections.OrderedDict(zip(dictionary.values(), dictionary.keys()))
-    result1, result2 = change_data_between_files(inverted_dict)
+    dic = collections.OrderedDict(zip(data1, data2))
+    new = collections.OrderedDict(zip(dic.values(), dic.keys()))
+    result1, result2 = change_data_between_files(new)
     return result1, result2
 
 
@@ -205,20 +204,20 @@ def find_and_replace_data(remote_url):
     return new_img
 
 
-def edit_page_for_offline_reading(text_str, remote_url, log_file):
+def offline_page(text_str, remote_url, log_file):
     with open(text_str, 'r') as f:
         data = f.read()
-        images = find_images_on_page(data, log_file)
+        images = find_images(data, log_file)
         new_img_urls = find_and_replace_data(remote_url)
-        dictionary = collections.OrderedDict(zip(images, new_img_urls))
-        for key, value in dictionary.items():
+        dic = collections.OrderedDict(zip(images, new_img_urls))
+        for key, value in dic.items():
             data = data.replace(key, value)
     with open(text_str, 'w') as new_file:
         new_file.write(data)
     return new_file
 
 
-def edit_page_for_comparing(text_str, local_data, remote_data):
+def comparing_content(text_str, local_data, remote_data):
     with open(text_str, 'r') as f:
         data = f.read()
     images = local_data
@@ -260,7 +259,7 @@ def read_urls(some_file, number, result):
     return result
 
 
-def read_urls_from_file(some_file):
+def url_from_file(some_file):
     address = []
     lines = count_lines(some_file)
     address = read_urls(some_file, lines, address)
@@ -283,45 +282,43 @@ def argument_control():
         addr = []
         log_file, urls_file, local_web_page, temporary_web_page = files()
         if sys.argv[1] == '-f':
-            addr = read_urls_from_file(urls_file)
+            addr = url_from_file(urls_file)
         else:
             addr.append(sys.argv[1])
     return log_file, urls_file, local_web_page, temporary_web_page, addr
 
 
-def save_web_page(web_page_url, log_file, local_html, directory_to_download, local_url, remote_url, item):
-    data_from_web_page = download_web_page_data(web_page_url, log_file)
-    print("Stahujem web stranku", item)
-    save_web_page_content(data_from_web_page, local_html, log_file)
-    print("Stahujem obrazky. Cakajte prosim.")
-    download_images_from_web_page(directory_to_download, data_from_web_page, web_page_url, log_file)
-    local_url, remote_url = get_and_change_data_in_files(local_url, remote_url)
-    edit_page_for_offline_reading(local_html, remote_url, log_file)
+def save_page(url, log, l_html, directory, l_url, r_url, item):
+    data = download_data(url, log)
+    print("Downloading web page", item)
+    save_content(data, l_html, log)
+    download_images(directory, data, url, log)
+    l_url, remote_url = change_files_data(l_url, r_url)
+    offline_page(l_html, r_url, log)
 
 
-def compare(local_url, remote_url, web_page_url, local_html, temporary_html, log_file):
-    local_img_list, remote_img_list = change_data_in_memory(local_url, remote_url)
-    edit_page_for_comparing(local_html, local_img_list, remote_img_list)
-    compare_web_page_content(web_page_url, local_html, temporary_html, log_file)
-    edit_page_for_comparing(local_html, remote_img_list, local_img_list)
+def compare(l_url, r_url, web_page_url, l_html, tmp, log):
+    l_img_list, r_img_list = change_data(l_url, r_url)
+    comparing_content(l_html, l_img_list, r_img_list)
+    compare_content(web_page_url, l_html, tmp, log)
+    comparing_content(l_html, r_img_list, l_img_list)
 
 
 def main():
-    log_file, urls_file, local_web_page, temporary_web_page, addr = argument_control()
+    log, urls_file, l_page, tmp, addr = argument_control()
     for item in addr:
-        web_page_url = check_correct_url(item)
-        directory_to_download = formated_url(web_page_url)
-        local_html = os.path.join(directory_to_download, local_web_page)
-        temporary_html = os.path.join(directory_to_download, temporary_web_page)
-        directory_presence = os.path.isdir(directory_to_download)
-        local_url, remote_url = stored_data(directory_to_download)
-
-        if directory_presence is not True:
-            make_aimed_directory(directory_to_download, log_file)
-            save_web_page(web_page_url, log_file, local_html, directory_to_download, local_url, remote_url, item)
+        url = check_correct_url(item)
+        directory = formated_url(url)
+        l_html = os.path.join(directory, l_page)
+        temp_html = os.path.join(directory, tmp)
+        dir_exist = os.path.isdir(directory)
+        l_url, r_url = stored_data(directory)
+        if dir_exist is not True:
+            make_dir(directory, log)
+            save_page(url, log, l_html, directory, l_url, r_url, item)
         else:
-            compare(local_url, remote_url, web_page_url, local_html, temporary_html, log_file)
-        print('Hotovo.')
+            compare(l_url, r_url, url, l_html, temp_html, log)
+        print('Finish.')
 
 
 main()
